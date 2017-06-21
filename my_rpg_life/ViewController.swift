2f53d4913406a892040e4662cb453f085d86cf91
Caio Cozza <caiocozza.art@gmail.com>
2017-06-20 21:09:35 -0300

    Initial Commit

---
+++ my_rpg_life/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  my_rpg_life
+//
+//  Created by Caio Cozza on 20/06/17.
+//  Copyright © 2017 Cora Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
