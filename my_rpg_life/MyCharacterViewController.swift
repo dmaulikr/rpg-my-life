9b725ce701086758dc67acb2fb3f28c8e2ea941d
Caio Cozza <caiocozza.art@gmail.com>
2017-06-21 22:26:39 -0300

    MyCharacter controller created

---
+++ my_rpg_life/MyCharacterViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MyCharacterViewController.swift
+//  my_rpg_life
+//
+//  Created by Caio Cozza on 21/06/17.
+//  Copyright © 2017 Cora Technology. All rights reserved.
+//
+
+import UIKit
+
+class MyCharacterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
