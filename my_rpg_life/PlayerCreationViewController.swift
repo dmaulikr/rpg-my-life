82bb88d371461d23ea0cf3565db1d57997c19ffe
Caio Cozza <caiocozza.art@gmail.com>
2017-06-24 15:57:24 -0300

    changes on character creation

--- my_rpg_life/PlayerCreationViewController.swift
+++ my_rpg_life/PlayerCreationViewController.swift
@@ -8,18 +8,49 @@
 
 import UIKit
 
-class PlayerCreationViewController: UIViewController {
+class PlayerCreationViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    @available(iOS 2.0, *)
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
 
+
+    @IBOutlet weak var nicknameText: UITextField!
+    @IBOutlet weak var raceText: UITextField!
+    let pickerView = UIPickerView()
+    var pickOption:[String] = ["", "Barbarian", "Bard", "Druid", "Mage", "Paladin", "Rogue"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        pickerView.delegate = self
+        
+        raceText.inputView = pickerView
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickOption.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickOption[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        raceText.text = pickOption[row]
+        self.view.endEditing(true);
+    }
+    
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let player = Player();
@@ -30,11 +61,15 @@ class PlayerCreationViewController: UIViewController {
         player.password = "4321";
         
         MyPlayer.myPlayer = player;
-        
-        /*if let mainViewController = segue.destination as? MainViewController {
-            mainViewController.player = player
-        }*/
     }
+    
+    @IBAction func nicknameEnd1(_ sender: UITextField) {
+        self.view.endEditing(true)
+    }
+    
+
+
+    
     
     @IBAction func backClicked(_ sender: Any) {
         self.dismiss(animated: true) {}
